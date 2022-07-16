f0dada80338470da2544d5a3c6c2cc8e49154d24
sarthakregmi <sarthakregmi18@gmail.com>
2022-07-16 15:27:11 +0545

    added api template

--- demo_website/accounts/models.py
+++ demo_website/accounts/models.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 from django.db import models
 
 # Create your models here.
@@ -7,5 +8,6 @@ class data_table(models.Model):
     password = models.CharField(max_length=100,null=True)
     type = models.CharField(max_length=100,null=True)
     email = models.CharField(max_length=100,null=True)
+    token = models.CharField(max_length=100,null=True)
     
     
--- demo_website/accounts/urls.py
+++ demo_website/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import RegisterAPI
 urlpatterns = [
     path('',views.home, name='home'),
-    path('register',views.register,name='register'),
+    # path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     path('api/register/', RegisterAPI.as_view(), name='register'),
 ]
